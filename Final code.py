16ca9708a7d4e86b7bedc178639bcc50c0e56a61
nglsu <79137365+nglsu@users.noreply.github.com>
2021-02-21 21:57:26 +0100

    Update Final code.py

--- Final code.py
+++ Final code.py
@@ -1,4 +1,4 @@
 # I N P U T S
-print("format: 1stData, 2ndData, 3rdData, 4thdata, 5thdata, 6thData, 7thData, 8thData, 9thData, 10thData")
+print("format: 1stData, 2ndData, 3rdData, 4thData, 5thData, 6thData, 7thData, 8thData, 9thData, 10thData")
 IU1, IU2, IU3, IU4, IU5, IU6, IU7, IU8, IU9, IU10 = input("enter your data: ").split(", ")
 print("\n", IU1, "\n", IU2, "\n", IU3, "\n", IU4, "\n", IU5, "\n", IU6, "\n", IU7, "\n", IU8, "\n", IU9, "\n", IU10)

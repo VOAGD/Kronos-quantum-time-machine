e7dd6388af64642e55bb1bb665a7648cc6d4eb83
nglsu <79137365+nglsu@users.noreply.github.com>
2021-02-21 21:53:02 +0100

    Update Final code.py

--- Final code.py
+++ Final code.py
@@ -1,1 +1,5 @@
-# why is this empty?
+# I N P U T S
+
+print("format: IU1, IU2, IU3, IU4, IU5, IU6, IU7, IU8, IU9, IU10")
+IU1, IU2, IU3, IU4, IU5, IU6, IU7, IU8, IU9, IU10 = input("enter your data: ").split(", ")
+print("\n", IU1, "\n", IU2, "\n", IU3, "\n", IU4, "\n", IU5, "\n", IU6, "\n", IU7, "\n", IU8, "\n", IU9, "\n", IU10)
